8c52af02faf7502279616b0daa12e7d1a07d2a58
Chris911 <Christophe.naud.dulude@gmail.com>
2013-12-04 21:18:40 -0500

    Format output

--- btc-e-total.rb
+++ btc-e-total.rb
@@ -15,7 +15,7 @@ my_currencies = info['return']['funds'].select { |currency, amount| amount > 0 }
 my_currencies.each do |currency, amount|
   if currency == "usd"
     sum += amount
-    puts "#{COLORS[:yellow]}#{currency} : #{COLORS[:cyan]}#{amount}"
+    puts "#{COLORS[:yellow]}#{currency.upcase} : #{COLORS[:cyan]}#{amount}"
   else
     if Btce::API::CURRENCY_PAIRS.include? "#{currency}_usd"
       ticker = Btce::Ticker.new "#{currency}_usd"
@@ -26,7 +26,9 @@ my_currencies.each do |currency, amount|
       usd_price  = ticker.last * ticker_btc.last
       sum += (usd_price * amount)
     end
-    printf "#{COLORS[:yellow]}%s : #{COLORS[:green]}%-13s #{COLORS[:cyan]} @ %s\n", currency, amount, ticker.last
+    usd_price.nil? ? exchange = "USD" : exchange = "BTC"
+    printf "#{COLORS[:yellow]}%s : #{COLORS[:green]}%-13s #{COLORS[:cyan]} @ %-8s #{COLORS[:yellow]}%s\n",
+           currency.upcase, amount, ticker.last, exchange
   end
 end
 
